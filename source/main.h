d253ef824b72f6c40b0bc500bbff80d744549527
酸柠檬猹 <SourLemonJuice233@outlook.com>
2024-06-15 16:53:10 +0800

    添加更多构建时的版本信息
    - 获取详细的(--long) git describe 输出
    - 显示本地构建或者远程CI构建
    - 重命名 CI 配置文件

--- source/main.h
+++ source/main.h
@@ -3,9 +3,16 @@
 
 #include <stdlib.h>
 
-/* For compiler defin flag(gcc -D) */
+/*
+    For compiler defin flag(gcc -D)
+    Those macros should be defined by compiler
+ */
 #ifndef APP_GIT_COMMIT_INFO
-#define APP_GIT_COMMIT_INFO "ERROR: APP_GIT_COMMIT_INFO should be defined by compiler"
+#define APP_GIT_COMMIT_INFO "[Build ERROR] APP_GIT_COMMIT_INFO not defined"
+#endif
+
+#ifndef APP_BUILD_HOST_DESCRIPTION
+#define APP_BUILD_HOST_DESCRIPTION "[Build ERROR] APP_BUILD_HOST_DESCRIPTION not defined"
 #endif
 
 /* Self define */
