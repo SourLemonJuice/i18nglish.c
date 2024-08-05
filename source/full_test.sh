2b819be5d5d8ede4d2043cd0fdd7b8d7435d4021
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2024-08-06 03:00:24 +0800

    修改 Linux 下最终二进制文件名
    - 还包括编译时 构建时间 的格式

--- source/full_test.sh
+++ source/full_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+i18nglish_exec="./i18nglish"
+
 if [[ ${OSTYPE} != "linux-gnu" ]]; then
     echo "Don't run it outside Linux, please.";
     return 1;
@@ -8,22 +10,22 @@ fi
 # remember, keep other command safe, but not this target. It's a Test.
 test_examples="'hello, world. -,hello --world,. --- lastOneNormal'"
 
-chmod +x ./i18nglish.out | exit $?
+chmod +x ${i18nglish_exec} | exit $?
 
 echo Raw: "${test_examples}"
 
 # args mode
-./i18nglish.out --mode arguments ${test_examples} || exit $?
+${i18nglish_exec} --mode arguments ${test_examples} || exit $?
 
 # stdin mode
-echo "${test_examples}" | ./i18nglish.out --mode stdin || exit $?
+echo "${test_examples}" | ${i18nglish_exec} --mode stdin || exit $?
 
 # invalid file path
-./i18nglish.out --mode file ./null
+${i18nglish_exec} --mode file ./null
 if [[ $? != 0 ]]; then
     echo "TEST: Program at least currectly exit with non zero"
 fi
 
 # file mode
 echo "${test_examples}" >| ./justForTest.tmp && \
-./i18nglish.out --mode file ./justForTest.tmp || exit $?
+${i18nglish_exec} --mode file ./justForTest.tmp || exit $?
